fe725c2fe11449df8b04158976cfd7abe44df5d6
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-05-03 18:47:39 +0300

    second commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', onGalleryListClick);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+function onGalleryListClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  // if (!e.target.classList.contains('gallery__image')) {
+  //   return;
+  // }
+  console.log(e.currentTarget);
+  console.log(e.target);
+}
 
 console.log(galleryItems);
